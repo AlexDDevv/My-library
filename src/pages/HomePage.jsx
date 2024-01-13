8bd8b3e1ffca8ae2d32b2871d4aff0c12e3d1b0f
Alexis <AlexDdevs@gmail.com>
2024-01-13 09:32:14 +0100

    modif architecture

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from '../components/Layouts/Header'
+import Footer from '../components/Layouts/Footer'
 import HomeContent from '../components/Home/HomeContent'
 
 export default function HomePage() {
     return (
-        <>
+        <div className="container">
+            <Header />
+            <Footer />
             <HomeContent />
-        </>
+        </div>
     )
 }
