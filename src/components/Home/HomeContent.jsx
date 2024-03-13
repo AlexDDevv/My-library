4de5ab3833194d5ab1314d2fcedcefa977a62241
Alexis <AlexDdevs@gmail.com>
2024-03-13 18:46:39 +0100

    call new component in HomeContent

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
 import bannerImg from "../../assets/images/banner2.webp"
+import data from "../../data/data.json"
+import Quote from '../Quotes/Quote'
 
 export default function HomeContent() {
+    const quote = data.quotes[0]
+
     return (
         <>
             <main className="home-content">
@@ -11,14 +15,11 @@ export default function HomeContent() {
                     location={"home-banner"}
                 />
                 <section className="home-section">
-                    <article className="title-quote">
-                        <h1 className="title">
-                            <q className='quote'>
-                                La lecture nourrit l'âme, comme le pain nourrit le corps.
-                            </q>
-                        </h1>
-                        <span className="autor">Antoine Albalat</span>
-                    </article>
+                    <Quote
+                        quote={quote.quote}
+                        author={quote.author}
+
+                    />
                     <article className="home-text">
                         <h2 className="welcome">Soyez les bienvenus chers lecteurs et lectrices !</h2>
                         <p className='home-p'>Depuis plusieurs mois maintenant, j'ai repris goût à la lecture. Ne pas voir le temps passer quand nous sommes plongés dans un livre, celui  qui nous instruit, nous fait rêver ou nous fait voyager. De belles émotions peuvent être transmises par la lecture, celle la même qui nourrit notre imaginaire, et celui des enfants.</p>
