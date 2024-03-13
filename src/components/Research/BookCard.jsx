9866838726f67bc95863226228670cfb2d1289b0
Alexis <AlexDdevs@gmail.com>
2024-03-13 08:46:15 +0100

    modif className container and change in scss files

--- src/components/Research/BookCard.jsx
+++ src/components/Research/BookCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BookCard({ thumbnail, title, author, onClick, showAddBtn, addBook, removeBtn, remove }) {
     return (
         <>
-            <article className='book' onClick={onClick}>
+            <article className='bookCard' onClick={onClick}>
                 <div className="book-cover">
                     <img
                         src={thumbnail}
