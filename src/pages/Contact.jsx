b802e7f23742ce3566a3eb55e80eba4ba93b5e3e
Alexis <AlexDdevs@gmail.com>
2024-03-13 18:48:12 +0100

    call components in Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from '../components/Header/Header'
 import ContactContent from '../components/Contact/ContactContent'
+import Footer from '../components/Footer/Footer'
 
 export default function Contact() {
     return (
-        <>
+        <div className="container">
+            <Header />
             <ContactContent />
-        </>
+            <Footer />
+        </div>
     )
 }
