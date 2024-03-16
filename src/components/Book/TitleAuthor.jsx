7aeba08366bf9cb4f7ebe5185b3efeebfdc16e59
Alexis <AlexDdevs@gmail.com>
2024-03-16 14:00:44 +0100

    add a subtitle for books

--- src/components/Book/TitleAuthor.jsx
+++ src/components/Book/TitleAuthor.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function TitleAuthor({ title, author }) {
+export default function TitleAuthor({ title, subtitle, author }) {
     return (
         <div className="title-author">
             <h2 className="title">{title}</h2>
+            {subtitle && (
+                <h3 className="subtitle">{subtitle}</h3>
+            )}
             <span className="author">{author}</span>
         </div>
     )
