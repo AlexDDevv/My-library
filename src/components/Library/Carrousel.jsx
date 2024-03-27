5b05b8f70c87747cbf15a0f8a9b58b22b4c10748
Alexis <AlexDdevs@gmail.com>
2024-03-27 16:55:51 +0100

    create an array in datas file for differents breakpoints and use it in a useEffect in Carrousel to remove books according to the screen width

--- src/components/Library/Carrousel.jsx
+++ src/components/Library/Carrousel.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import BookCard from '../Book/BookCard'
 import BtnCarrousel from './BtnCarrousel'
 import datas from "../../data/data.json"
 
 export default function Carrousel({ book, setBook, showBook, setShowBook, setSelectedBook }) {
     const [index, setIndex] = useState(0)
+    const [nbBooksToShow, setNbBooksToShow] = useState(5);
+
+    useEffect(() => {
+        function handleResize() {
+            const breakpoints = datas.breakPoints.sort((a, b) => b.maxWidth - a.maxWidth);
+            const { numBooks } = breakpoints.find(bp => window.innerWidth > bp.maxWidth) || breakpoints[breakpoints.length - 1];
+            setNbBooksToShow(numBooks);
+        }
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const renderBookFocus = (book) => {
         setSelectedBook(book)
         setShowBook(!showBook)
     }
     const renderBookFocusKey = (e, book) => {
-		if (e.key === "Enter") {
-			renderBookFocus(book)
-		}
-	}
+        if (e.key === "Enter") {
+            renderBookFocus(book)
+        }
+    }
 
     const nextBook = () => {
         setIndex((prevIndex) => (prevIndex + 1) % book.length);
@@ -33,30 +46,36 @@ export default function Carrousel({ book, setBook, showBook, setShowBook, setSel
 
     return (
         <div className="books-container">
-            {[...book.slice(index), ...book.slice(0, index)].slice(0, 5).map((book) => (
-                <BookCard
-                    key={book.id}
-                    onClick={() => renderBookFocus(book)}
-					onKeyDown={(e) => renderBookFocusKey(e, book)}
-                    thumbnail={book.volumeInfo.imageLinks?.smallThumbnail ?? 'URL_PAR_DEFAUT'}
-                    bookName={book.volumeInfo.title}
-                    title={book.volumeInfo.title}
-                    author={book.volumeInfo.authors}
-                    removeBtn={true}
-                    remove={(e) => removeBookFromLibrary(book.id, e)}
+            <div className="view-books">
+                {[...book.slice(index), ...book.slice(0, index)].slice(0, nbBooksToShow).map((book) => (
+                    <BookCard
+                        key={book.id}
+                        onClick={() => renderBookFocus(book)}
+                        onKeyDown={(e) => renderBookFocusKey(e, book)}
+                        thumbnail={book.volumeInfo.imageLinks?.smallThumbnail ?? 'URL_PAR_DEFAUT'}
+                        bookName={book.volumeInfo.title}
+                        title={book.volumeInfo.title}
+                        author={book.volumeInfo.authors}
+                        removeBtn={true}
+                        remove={(e) => removeBookFromLibrary(book.id, e)}
 
-                />
-            ))}
-            {book.length > 5 && (
-                datas.btnCarrousel.map((item, i) => (
-                    <BtnCarrousel
-                        key={i}
-                        side={item.className}
-                        onClick={item.className === "carrousel-btn previous" ? prevBook : nextBook}
-                        icon={item.icon}
-                        span={item.span}
                     />
-                ))
+                ))}
+            </div>
+            {book.length > nbBooksToShow && (
+                <div className="change-books">
+                    {
+                        datas.btnCarrousel.map((item, i) => (
+                            <BtnCarrousel
+                                key={i}
+                                side={item.className}
+                                onClick={item.className === "carrousel-btn previous" ? prevBook : nextBook}
+                                icon={item.icon}
+                                span={item.span}
+                            />
+                        ))
+                    }
+                </div>
             )}
         </div>
     )
