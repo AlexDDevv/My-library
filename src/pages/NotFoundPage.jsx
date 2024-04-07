90e37cf9f6b04209471af478817786e6806e12ce
Alexis <AlexDdevs@gmail.com>
2024-04-07 14:26:26 +0200

    Add error page in Routes

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFound from '../components/NotFound/NotFound'
+
+export default function NotFoundPage() {
+    return (
+        <div className="container for-notFound">
+            <NotFound />
+        </div>
+    )
+}
