e1bd4c6a997600d3aac827d27e265daa844901ee
Alexis <AlexDdevs@gmail.com>
2024-04-20 10:10:05 +0200

    add type "button" to BookBtn add a better alt for banner img change title size in research page

--- src/components/Book/BookBtn.jsx
+++ src/components/Book/BookBtn.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function BookBtn({ addOrRemove, contentBtn }) {
     return (
-        <button className="add-book" onClick={addOrRemove}>{contentBtn}</button>
+        <button type='button' className="add-book" onClick={addOrRemove}>{contentBtn}</button>
     )
 }
